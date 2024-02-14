0d17a6471e61c053b29fc255469232c037ffad34
GenviMK240 <genvi240@gmail.com>
2024-02-14 12:41:36 +0100

    Update spearheads.R

---
+++ spearheads.R
@@ -0,0 +1,37 @@
+getwd()
+nuevo_dir<-"C:/spearheads practica 4"
+setwd(nuevo_dir)
+
+spear<-read_excel("C:/Descargas Google/spearheads.xlsx")
+View(spear)
+str(spear)
+class(spear)
+  
+  
+  as.data.frame(spear)
+
+names(spear)[names(spear)== "Mat"]<-"Materiales"
+names(spear)[names(spear)== "Con"]<-"Contexto"
+names(spear)[names(spear)== "Cond"]<-"Conservación"
+names(spear)[names(spear)== "Loo"]<-"Loop"
+names(spear)[names(spear)== "Peg"]<-"Remache"
+names(spear)[names(spear)== "Date"]<-"Fecha"
+names(spear)[names(spear)== "Maxle"]<-"Longitud_max"
+names(spear)[names(spear)== "Socle"]<-"Longitud_encaje"
+names(spear)[names(spear)== "Maxwit"]<-"Ancho_max_encaje"
+names(spear)[names(spear)== "Weight"]<-"Peso"
+
+spear
+View(spear)
+
+#3
+spear$Contexto=factor(spear$Contexto,levels=c('1', '2', '3'), labels=c("s/c", "Habitacional", "Funerario"))
+spear$Conservación=factor(spear$Conservación,levels=c(1,2,3,4), labels=c("Excelente", "Bueno", "Regular", "Malo"))
+spear$Remache=factor(spear$Remache,levels=c(1,2), labels=c('Si', 'No'))
+spear$Materiales=factor(spear$Materiales,levels=c(1,2), labels=c('Bronce', 'Hierro'))
+
+View(spear)
+
+
+
+
